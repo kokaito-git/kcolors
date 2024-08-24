cb030968a8672201774ea58c8a512c49aa0c3298
kokaito-git <kokaito.git@gmail.com>
2024-08-24 20:17:14 +0200

    restored deleted files

--- kcolors/src/colors.py
+++ kcolors/src/colors.py
@@ -30,7 +30,7 @@ class SColors:
     END = "\033[0m"  # Reset all formatting
 
 
-class Colors(SColors):
+class Colors:
     """
     A class for automatic color codes and text formatting for terminal output.
 
@@ -55,6 +55,36 @@ class Colors(SColors):
     - If you want to avoid automatic disabling of colors when output is not to a terminal, use the `StatiColors` class instead, which provides static attributes and does not automatically disable colors based on the terminal status.
     """
 
+    # Regular colors
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    YELLOW = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    GRAY = "\033[0;37m"
+
+    # Bold colors (bright versions)
+    BBLACK = "\033[1;30m"
+    BRED = "\033[1;31m"
+    BGREEN = "\033[1;32m"
+    BYELLOW = "\033[1;33m"
+    BBLUE = "\033[1;34m"
+    BPURPLE = "\033[1;35m"
+    BCYAN = "\033[1;36m"
+    BGRAY = "\033[1;37m"
+
+    # Text formatting
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"  # Reset all formatting
+
     # Handle cases when output is not to a terminal
     if not __import__("sys").stdout.isatty():
         for _ in dir():
@@ -66,4 +96,3 @@ class Colors(SColors):
             kernel32 = __import__("ctypes").windll.kernel32
             kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
             del kernel32
-
